1420072a87cb6f63760ca82ef8d98253f800d7b1
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-02-24 03:52:36 +0530

    add propTypes for CustomButton

--- week-05/week-5.1/counter-app/src/App.jsx
+++ week-05/week-5.1/counter-app/src/App.jsx
@@ -1,6 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import PropTypes from 'prop-types'
 import './App.css'
 
+function CustomButton(props) {
+
+  // function onClickHandler() {    // dont know why but this was also running
+  //   setCount(count + 1)
+  // }
+
+  function onClickHandler() {
+    props.setCount(props.count + 1)
+  }
+
+  return <button onClick={onClickHandler }>
+  Counter {props.count}
+  </button>
+}
+
+CustomButton.propTypes = {
+  count: PropTypes.number.isRequired,
+  setCount: PropTypes.func.isRequired
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,20 +31,7 @@ function App() {
     </div>
   )
 
-  function CustomButton(props) {
 
-    // function onClickHandler() {    // dont know why but this was also running
-    //   setCount(count + 1)
-    // }
-
-    function onClickHandler() {
-      props.setCount(props.count + 1)
-    }
-  
-    return <button onClick={onClickHandler }>
-    Counter {props.count}
-    </button>
-  }
 }
 
 export default App
