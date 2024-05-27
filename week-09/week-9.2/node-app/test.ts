9220bdb7c62a6b500451397d1f82807b9238df16
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-28 03:54:01 +0530

    basic typescript

---
+++ week-09/week-9.2/node-app/test.ts
@@ -0,0 +1,40 @@
+// const x: number = 10;
+// console.log(x)
+
+
+// ---- how to provide argument to a function -------------
+
+// function greet(firstname: string) {
+//     console.log("Hello " + firstname);
+// }
+
+// greet("Manish")
+
+
+
+// ------ create a sum function -----------
+
+// function sum(a: number, b: number) {
+//     let sum: number = a + b
+//     console.log("Sum = " + sum)
+// }
+// sum(20, 5)
+
+
+// function sum(a: number, b: number): number {
+//    return a + b
+// }
+// const ans = sum(20, 5)
+// console.log(ans)
+
+
+
+// ----- create a function that takes another function as an input and runs it after n sec
+
+function runAfter1S(func: () => void) {           // void because it returns nothing
+    setTimeout(func, 5000)
+}
+
+runAfter1S(function(){
+    console.log('hello')
+})
