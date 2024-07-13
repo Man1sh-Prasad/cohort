7d1238e7252e7a3f5ab7cc2efda9a3d773506742
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-07-14 03:55:59 +0530

    add commnet

--- week-10/week-10.2/src/types.ts
+++ week-10/week-10.2/src/types.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+// By declaring userId in the Request interface extension (express.d.ts), TypeScript recognizes userId as a valid property for Request objects.
 declare module 'express' {
     interface Request {
         userId?: number; // Define your custom property here
