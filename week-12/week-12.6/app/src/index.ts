f1ee7ae7d28ed7074148bf71f4dbf6d32025e884
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-08-15 23:36:19 +0530

    connection pooling in serverless envs

---
+++ week-12/week-12.6/app/src/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate"; 
+
+export interface Env {
+	DATABASE_URL: string
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_URL,
+		}).$extends(withAccelerate())
+
+		const response = await prisma.log.create({
+			data: {
+				level: 'Info',
+				message: `${request.method} ${request.url}`,
+				meta: {
+					headers: JSON.stringify(request.headers),
+				}
+			}
+		})
+
+		const { data, info } = await prisma.log.findMany({
+			take: 20,
+			orderBy: {
+				id: "desc",
+			}
+		}).withAccelerateInfo()
+
+		console.log(JSON.stringify(info))
+
+		return Response.json(response)
+	}
+}
